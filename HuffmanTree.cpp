8a9af1d08bf18cc6cb03c9ed07cd93b471d523ce
Paul-Rip <paul_rip@yahoo.com>
2025-05-26 18:16:34 -0700

    Update HuffmanTree.cpp

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -6,7 +6,10 @@
 
 HuffmanTree::HuffmanTree(std::map<char, int> counts){
     priority_queue<HuffmanNode*, vector<HuffmanNode*>, HuffmanNode::Compare> pq;
-    pq.push(new HuffmanNode('@', 1));
+    pq.push(new HuffmanNode(EOF_marker, 1));
+    if(counts.empty()){
+        throw "Counts map can not be empty";
+    }
     for(auto kv : counts){
         pq.push(new HuffmanNode(kv.first, kv.second));
     }
@@ -27,6 +30,9 @@ HuffmanTree::HuffmanTree(std::istream* in){
     char c;
     root = new HuffmanNode('/', 0);
     HuffmanNode* curr = root;
+    if(in == nullptr){
+        throw "file is not valid";
+    }
     while(getline(*in, line)){
         if(x % 2 == 0){
             c = static_cast<char>(std::stoi(line));
@@ -51,31 +57,44 @@ HuffmanTree::HuffmanTree(std::istream* in){
         x++;
     }
 }
-std::map<char, std::string> HuffmanTree::createEncodingsHelper(HuffmanNode* root, std::string path, int i){
+std::map<char, std::string> HuffmanTree::createEncodingsHelper(HuffmanNode* root, std::string path){
     if(root == nullptr){
         return paths;
     }
     if(root->isLeaf()){
         paths[root->getC()] = path;
-        std::cout << root->getC() << " " << path << endl;
     } else{
-        createEncodingsHelper(root->getLeft(), path + "0", i);
-        createEncodingsHelper(root->getRight(), path + "1", i);
+        createEncodingsHelper(root->getLeft(), path + "0");
+        createEncodingsHelper(root->getRight(), path + "1");
     }
     return paths;
 }
 std::map<char, std::string> HuffmanTree::createEncodings(){
-    createEncodingsHelper(root, "", 0);
-    return paths;
+    map<char, std::string> allPaths;
+    if(root == nullptr){
+        throw "Tree can not be empty";
+    }
+    createEncodingsHelper(root, "");
+    for(auto kv : paths){
+        if(kv.first != EOF_marker){
+            allPaths[kv.first] = kv.second;
+        }else{
+            allPaths[(char)EOF_marker] = paths[EOF_marker];
+        }
+    }
+    return allPaths;
 }
 void HuffmanTree::compress(ifstream* input, OBitStream* output){
     char c;
     std::string pathOfC;
+    if(input == nullptr || output == nullptr){
+        throw "At least one file is not valid";
+    }
     while(input->get(c)){
         pathOfC = paths[c];
         output->writeBits(pathOfC);
     }
-    pathOfC = paths['@'];
+    pathOfC = paths[EOF_marker];
     output->writeBits(pathOfC);
     output->close();
 }
@@ -83,19 +102,32 @@ void HuffmanTree::compress(ifstream* input, OBitStream* output){
 void HuffmanTree::decompress(IBitStream* input, OBitStream* output){
     HuffmanNode* current = root;
     int bit;
-    while((bit = input->readBit()) != -1){
+    bool eof = false;
+    if(input == nullptr || output == nullptr){
+        throw "At least one file is not valid";
+    }
+    while((bit = input->readBit()) != -1 && !eof){
          if(bit == 0){
             cout << "left" << endl;
             current = current->getLeft();
         }else if (bit == 1){
             cout << "right" << endl;
             current = current->getRight();
+        }else if(bit == -1){
+            return;
         }
 
         if(current->isLeaf()){
-            cout << current->getC() << endl;
-            output->write(current->getC());
-            current = root;
+            if(current->getC() == EOF_marker){
+                eof = true;
+            }else{
+                cout << current->getC() << endl;
+                output->write(current->getC());
+                current = root;
+            }
+
         }
     }
+
+    output->close();
 }
